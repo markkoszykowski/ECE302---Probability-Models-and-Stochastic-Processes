22f9fbde6a57f8d07309f478bfe707c91377ee1d
Mark Koszykowski <markkoszykowski@gmail.com>
2021-03-17 23:36:01 -0400

    corrected graph

--- Project 2/p2.m
+++ Project 2/p2.m
@@ -31,65 +31,64 @@ sc1_table = table(Rows, LMMSE, BMMSE)
 % params
 N = 1e6;
 N_obs = 20;
+obs = 1:1:N_obs;
 Y_var = [.5, .75, .75];
 R_var = [1, .75, .5];
 
 % theoretical MSEs
-MMSE1 = (Y_var(1) * R_var(1)) / (N_obs * Y_var(1) + R_var(1));
-MMSE2 = (Y_var(2) * R_var(2)) / (N_obs * Y_var(2) + R_var(2));
-MMSE3 = (Y_var(3) * R_var(3)) / (N_obs * Y_var(3) + R_var(3));
+MMSE1 = (Y_var(1) .* R_var(1)) ./ (obs .* Y_var(1) + R_var(1));
+MMSE2 = (Y_var(2) .* R_var(2)) ./ (obs .* Y_var(2) + R_var(2));
+MMSE3 = (Y_var(3) .* R_var(3)) ./ (obs .* Y_var(3) + R_var(3));
 
-% generating random vectors
-Y1 = normrnd(1, sqrt(Y_var(1)), [N 1]);
-R1 = normrnd(0, sqrt(R_var(1)), [N N_obs]);
-X1 = zeros(N, N_obs);
-for i = 1:N_obs
-    X1(:, i) = R1(:, i) + Y1;
+for n = obs
+    % generating random vectors
+    Y1 = normrnd(1, sqrt(Y_var(1)), [N 1]);
+    R1 = normrnd(0, sqrt(R_var(1)), [N n]);
+    X1 = zeros(N, n);
+    for i = 1:n
+        X1(:, i) = R1(:, i) + Y1;
+    end
+
+    Y2 = normrnd(1, sqrt(Y_var(2)), [N 1]);
+    R2 = normrnd(0, sqrt(R_var(2)), [N n]);
+    X2 = zeros(N, n);
+    for i = 1:n
+        X2(:, i) = R2(:, i) + Y2;
+    end
+
+    Y3 = normrnd(1, sqrt(Y_var(3)), [N 1]);
+    R3 = normrnd(0, sqrt(R_var(3)), [N n]);
+    X3 = zeros(N, n);
+    for i = 1:n
+        X3(:, i) = R3(:, i) + Y3;
+    end
+
+    % calculate errors
+    y1_hat = mmmse(X1, Y1);
+    MMSE1(2, n) = mean((Y1 - y1_hat) .^ 2);
+
+    y2_hat = mmmse(X2, Y2);
+    MMSE2(2, n) = mean((Y2 - y2_hat) .^ 2);
+
+    y3_hat = mmmse(X3, Y3);
+    MMSE3(2, n) = mean((Y3 - y3_hat) .^ 2);
 end
-
-Y2 = normrnd(1, sqrt(Y_var(2)), [N 1]);
-R2 = normrnd(0, sqrt(R_var(2)), [N N_obs]);
-X2 = zeros(N, N_obs);
-for i = 1:N_obs
-    X2(:, i) = R2(:, i) + Y2;
-end
-
-Y3 = normrnd(1, sqrt(Y_var(3)), [N 1]);
-R3 = normrnd(0, sqrt(R_var(3)), [N N_obs]);
-X3 = zeros(N, N_obs);
-for i = 1:N_obs
-    X3(:, i) = R3(:, i) + Y3;
-end
-
-% calculate errors
-y1_hat = mmmse(X1, Y1);
-MMSE1(2, 1) = mean((Y1 - y1_hat) .^ 2);
-
-y2_hat = mmmse(X2, Y2);
-MMSE2(2, 1) = mean((Y2 - y2_hat) .^ 2);
-
-y3_hat = mmmse(X3, Y3);
-MMSE3(2, 1) = mean((Y3 - y3_hat) .^ 2);
-
-Rows = {'Theoretical'; 'Experimental'};
 
 % generate plot
-tot = [MMSE1 MMSE2 MMSE3];
-
-x = linspace((min(tot, [], 'all') - .25 * (max(tot, [], 'all') - min(tot, [], 'all'))), ...
-    (max(tot, [], 'all') + .25 * (max(tot, [], 'all') - min(tot, [], 'all'))));
 
 figure;
-plot(x, x);
-hold on;
-scatter(tot(1, :), tot(2, :));
-for i = 1:length(Y_var)
-    text(tot(1, i) - .003, tot(2, i) + .0018, "\sigma_{\it Y}^2 = " + Y_var(i) + ", \sigma_{\it R}^2 = " + R_var(i));
-end
-xlim([min(x) max(x)]);
-title("Comparison of Theoretical and Experimental MMSE with " + N_obs + " observations");
-xlabel("Theoretical MMSE Results");
-ylabel("Experimental MMSE Results");
-
-% generate table
-sc1_table = table(Rows, MMSE1, MMSE2, MMSE3)
+plot(obs, MMSE1(1, :), '-', ...
+    obs, MMSE1(2, :), 'x', ...
+    obs, MMSE2(1, :), '-', ...
+    obs, MMSE2(2, :), 'x', ...
+    obs, MMSE3(1, :), '-', ...
+    obs, MMSE3(2, :), 'x');
+title("Theoretical and Experimental MSE vs. Number of Observations");
+xlabel("Number of Observations");
+ylabel("MSE");
+legend("Theoretical: \sigma_{\it Y}^2 = " + Y_var(1) + ", \sigma_{\it R}^2 = " + R_var(1), ...
+    "Experimental: \sigma_{\it Y}^2 = " + Y_var(1) + ", \sigma_{\it R}^2 = " + R_var(1), ...
+    "Theoretical: \sigma_{\it Y}^2 = " + Y_var(2) + ", \sigma_{\it R}^2 = " + R_var(2), ...
+    "Experimental: \sigma_{\it Y}^2 = " + Y_var(2) + ", \sigma_{\it R}^2 = " + R_var(2), ...
+    "Theoretical: \sigma_{\it Y}^2 = " + Y_var(3) + ", \sigma_{\it R}^2 = " + R_var(3), ...
+    "Experimental: \sigma_{\it Y}^2 = " + Y_var(3) + ", \sigma_{\it R}^2 = " + R_var(3));
