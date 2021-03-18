7d2858faa2c828dccd661bf7668baf3bd05d28ba
Mark Koszykowski <markkoszykowski@gmail.com>
2021-03-18 02:44:04 -0400

    update variance of R calculation

--- Project 2/mmmse.m
+++ Project 2/mmmse.m
@@ -2,10 +2,10 @@ function [MMMSE] = mmmse(X,Y)
     muY = mean(Y);
     varY = var(Y);
     [N, N_obs] = size(X);
-    varR = zeros(N_obs, 1);
+    varR = zeros(N, N_obs);
     for i = 1:N_obs
-        varR(i) = var((X(:, i) - Y));
+        varR(:, i) = X(:, i) - Y;
     end
-    avgVarR = mean(varR);
+    avgVarR = var(reshape(varR, [], 1));
     MMMSE = (1 / (N_obs * varY + avgVarR)) * (avgVarR * muY + varY * sum(X, 2));
 end
