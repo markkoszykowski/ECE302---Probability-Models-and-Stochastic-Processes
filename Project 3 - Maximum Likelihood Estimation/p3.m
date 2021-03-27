0fe1327939157dba19f0854c5355f567617aec22
Mark Koszykowski <markkoszykowski@gmail.com>
2021-03-27 19:37:26 -0400

    added part 1

--- Project 3 - Maximum Likelihood Estimation/p3.m
+++ Project 3 - Maximum Likelihood Estimation/p3.m
@@ -3,5 +3,99 @@ clc; clear; close all;
 
 %% Part 1
 
+% Parameters for Part 1
+N = 1e6;
+N_obs = 20;
+obs = 5:25;
+lambda = [.5 1];
+alpha = [.5 1];
+
+% Arrays to store MSE for each # of observations
+mseExp1 = zeros(size(obs));
+mseExp2 = zeros(size(obs));
+mseRay1 = zeros(size(obs));
+mseRay2 = zeros(size(obs));
+
+% Arrays to store bias for each # of observations
+biasExp1 = zeros(size(obs));
+biasExp2 = zeros(size(obs));
+biasRay1 = zeros(size(obs));
+biasRay2 = zeros(size(obs));
+
+% Arrays to store variance for each # of observations
+varExp1 = zeros(size(obs));
+varExp2 = zeros(size(obs));
+varRay1 = zeros(size(obs));
+varRay2 = zeros(size(obs));
+
+for i = obs
+    % Draw i samples from the respective distribution N times
+    exp1 = exprnd(1/lambda(1), [N i]);
+    exp2 = exprnd(1/lambda(2), [N i]);
+    ray1 = raylrnd(alpha(1), [N i]);
+    ray2 = raylrnd(alpha(2), [N i]);
+    
+    % Calculate maximum likelihood for each N attempt
+    %
+    % Exponential Distribution:
+    %   ^lambda^ = n / Sigma(i = 0, n) {xi}
+    %
+    % Rayleigh Distribution:
+    %   ^alpha^ = sqrt(1/2n * Sigma(i = 0, n) {xi})
+    %
+    lambdaHat1 = i ./ sum(exp1, 2);
+    lambdaHat2 = i ./ sum(exp2, 2);
+    alphaHat1 = sqrt(.5 * mean(ray1.^2, 2));
+    alphaHat2 = sqrt(.5 * mean(ray2.^2, 2));
+    
+    % Calculate MSE for each distribution
+    mseExp1(i - obs(1) + 1) = mean((lambda(1) - lambdaHat1).^2);
+    mseExp2(i - obs(1) + 1) = mean((lambda(2) - lambdaHat2).^2);
+    mseRay1(i - obs(1) + 1) = mean((alpha(1) - alphaHat1).^2);
+    mseRay2(i - obs(1) + 1) = mean((alpha(2) - alphaHat2).^2);
+    
+    % Calculate bias for each distribution
+    biasExp1(i - obs(1) + 1) = mean((lambdaHat1 - lambda(1)));
+    biasExp2(i - obs(1) + 1) = mean((lambdaHat2 - lambda(2)));
+    biasRay1(i - obs(1) + 1) = mean((alphaHat1 - alpha(1)));
+    biasRay2(i - obs(1) + 1) = mean((alphaHat2 - alpha(2)));
+    
+    % Calculate variance for each distribution
+    varExp1(i - obs(1) + 1) = var(lambdaHat1);
+    varExp2(i - obs(1) + 1) = var(lambdaHat2);
+    varRay1(i - obs(1) + 1) = var(alphaHat1);
+    varRay2(i - obs(1) + 1) = var(alphaHat2);
+end
+
+% Create plots
+figure;
+plot(obs, mseExp1, obs, mseExp2, obs, mseRay1, obs, mseRay2);
+title("MSE of Paramter \theta vs. Number of Observation");
+xlabel("Number of Observations");
+ylabel("MSE");
+legend("Exponential RV: \lambda = " + lambda(1), ...
+    "Exponential RV: \lambda = " + lambda(2), ...
+    "Rayleigh RV: \alpha = " + alpha(1), ...
+    "Rayleigh RV: \alpha = " + alpha(2));
+
+figure;
+plot(obs, biasExp1, obs, biasExp2, obs, biasRay1, obs, biasRay2);
+title("Bias of Paramter \theta vs. Number of Observation");
+xlabel("Number of Observations");
+ylabel("Bias");
+legend("Exponential RV: \lambda = " + lambda(1), ...
+    "Exponential RV: \lambda = " + lambda(2), ...
+    "Rayleigh RV: \alpha = " + alpha(1), ...
+    "Rayleigh RV: \alpha = " + alpha(2));
+
+figure;
+plot(obs, varExp1, obs, varExp2, obs, varRay1, obs, varRay2);
+title("Variance of Paramter \theta vs. Number of Observation");
+xlabel("Number of Observations");
+ylabel("Variance");
+legend("Exponential RV: \lambda = " + lambda(1), ...
+    "Exponential RV: \lambda = " + lambda(2), ...
+    "Rayleigh RV: \alpha = " + alpha(1), ...
+    "Rayleigh RV: \alpha = " + alpha(2));
 
 %% Part 2
